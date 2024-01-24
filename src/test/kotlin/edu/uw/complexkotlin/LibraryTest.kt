b7cff9009f17b80d11dd2d1c82e3868d16e137fd
SamRitian <xyou@uw.edu>
2024-01-23 23:43:42 -0800

    extra credit 1

--- src/test/kotlin/edu/uw/complexkotlin/LibraryTest.kt
+++ src/test/kotlin/edu/uw/complexkotlin/LibraryTest.kt
@@ -6,37 +6,61 @@ package edu.uw.complexkotlin
 import kotlin.test.*
 
 class LibraryTest {
-    @Test fun fizzbuzzToTwo() {
+    @Test
+    fun fizzbuzzToTwo() {
         assertEquals("", fizzbuzz(1..2))
     }
-    @Test fun fizzbuzzToThree() {
+    @Test
+    fun fizzbuzzToThree() {
         assertEquals("FIZZ", fizzbuzz(1..3))
     }
-    @Test fun fizzbuzzToSix() {
+    @Test
+    fun fizzbuzzToSix() {
         assertEquals("FIZZBUZZFIZZ", fizzbuzz(1..6))
     }
-    @Test fun fizzbuzzToTwelve() {
+    @Test
+    fun fizzbuzzToTwelve() {
         assertEquals("FIZZBUZZFIZZFIZZBUZZFIZZ", fizzbuzz(1..12))
     }
-    @Test fun fizzbuzzToFifteen() {
+    @Test
+    fun fizzbuzzToFifteen() {
         assertEquals("FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ", fizzbuzz(1..15))
     }
-    // Fizzbuzz to 25 should equal "FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZ"
-    // Fizzbuzz to 50 should equal "FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZ"
+    @Test
+    fun fizzbuzzToTwentyFive() {
+        assertEquals("FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZ", fizzbuzz(1..25))
+    }
+    @Test
+    fun fizzbuzzToFifty() {
+        assertEquals(
+                "FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZ",
+                fizzbuzz(1..50)
+        )
+    }
+    @Test
+    fun fizzbuzzToOneHundred() {
+        assertEquals(
+                "FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZ",
+                fizzbuzz(1..100)
+        )
+    }
 
-    //@Test fun fizzbuzzgenToFifteen() {
+    // @Test fun fizzbuzzgenToFifteen() {
     //    val fb = fizzbuzzgen(mapOf(3 to "FIZZ", 5 to "BUZZ"))
     //    assertEquals("FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ", fb(1..15))
-    //}
+    // }
 
-    @Test fun r1Test() {
+    @Test
+    fun r1Test() {
         assertEquals(">>> FOO: {BAR}", r1())
     }
-    @Test fun r2Test() {
+    @Test
+    fun r2Test() {
         assertEquals(">>> FOO: {WOOGAWOOGAWOOGA}", r2())
     }
 
-    @Test fun philosopherTests() {
+    @Test
+    fun philosopherTests() {
         var seneca = Philosopher.THINKING
         assertEquals("Deep thoughts....", seneca.toString())
 
@@ -53,7 +77,8 @@ class LibraryTest {
         assertEquals("Allow me to suggest an idea...", seneca.toString())
     }
 
-    @Test fun commandTests() {
+    @Test
+    fun commandTests() {
         assertEquals("", Command("")(""))
         assertEquals("> Hello!", Command("> ")("Hello!"))
     }
